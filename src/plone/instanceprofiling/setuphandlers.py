b5c7149ac5d21924af8e4a102c7920ae349c5e1c
Johannes Raggam <thetetet@gmail.com>
2017-02-02 17:49:51 +0100

    progress

--- src/plone/instanceprofiling/setuphandlers.py
+++ src/plone/instanceprofiling/setuphandlers.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+from zope.component.hooks import getSite
+from collective.loremipsum.utils import create_subobjects
+
+
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
+    portal = getSite()
+	import pdb; pdb.set_trace()
+    create_subobjects(
+        portal,
+        portal,
+        {
+           'portal_type': ['Page', 'News Item', 'Event'],
+           'amount': 100,
+           'recurse': True,
+           'recursion_depth': 3,
+           'recurse_same_ptypes': True,
+           'publish': True,
+        }
+    )
